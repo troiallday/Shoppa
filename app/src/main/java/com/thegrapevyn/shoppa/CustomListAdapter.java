0118b03a112f2130ea2757279d62315da3555169
troiallday <tapiwalouis3@gmail.com>
2017-01-27 03:34:07 +0200

    Initial commit

---
+++ app/src/main/java/com/thegrapevyn/shoppa/CustomListAdapter.java
@@ -0,0 +1,110 @@
+package com.thegrapevyn.shoppa;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by TapiwaLouis on 25/1/2017.
+ */
+
+public class CustomListAdapter  extends ArrayAdapter<String> {
+
+    private InputStream OpenHttpConnection(String urlString) throws IOException
+    {
+        InputStream in = null;
+        int response = -1;
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+        if (!(conn instanceof HttpURLConnection))
+            throw new IOException("Not an HTTP connection");
+        try{
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            response = httpConn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new IOException("Error connecting");
+        }
+        return in;
+    }
+
+
+
+    private  MainActivity context;
+    private  String[] prod_name;
+    private  String [] prod_price;
+    private  String [] prod_image;
+
+
+
+    public CustomListAdapter(MainActivity context, String[] prod_name, String[] prod_price, String[] prod_image) {
+        super(context, R.layout.mylist, prod_name);
+        // TODO Auto-generated constructor stub
+
+        this.context=context;
+        this.prod_name=prod_name;
+        this.prod_price=prod_price;
+        this.prod_image=prod_image;
+
+    }
+
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        final View rowView = inflater.inflate(R.layout.mylist, null, true);
+
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.item);
+        final ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+        TextView extratxt = (TextView) rowView.findViewById(R.id.textView1);
+
+        txtTitle.setText(prod_name[position]);
+        extratxt.setText("Price: $" + prod_price[position]);
+        imageView.setImageBitmap(DownloadImage("http://192.168.1.2/troi_one/product_images/image1.jpg"));
+
+        //-------------------------------------------------------------------------
+
+        //Bitmap bitmap = DownloadImage("http://192.168.1.2/troi_one/product_images/image1.jpg");
+
+        //---------------------------------------------------------------------------
+
+
+        return rowView;
+    }
+
+    private Bitmap DownloadImage(String s) {
+        Bitmap bitmap = null;
+        InputStream in = null;
+        try {
+            in = OpenHttpConnection(s);
+            bitmap = BitmapFactory.decodeStream(in);
+            in.close();
+        } catch (IOException e1) {
+            Toast.makeText(getContext(), e1.getLocalizedMessage(),
+                    Toast.LENGTH_LONG).show();
+            e1.printStackTrace();
+        }
+        return bitmap;
+    }
+
+
+}
---
+++ app/src/main/java/com/thegrapevyn/shoppa/MainActivity.java
@@ -0,0 +1,146 @@
+package com.thegrapevyn.shoppa;
+
+import android.app.ListActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    private InputStream OpenHttpConnection(String urlString) throws IOException
+    {
+        InputStream in = null;
+        int response = -1;
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+        if (!(conn instanceof HttpURLConnection))
+            throw new IOException("Not an HTTP connection");
+        try{
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            response = httpConn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new IOException("Error connecting");
+        }
+        return in;
+    }
+
+
+    private String DownloadText(String URL)
+    {
+        int BUFFER_SIZE = 2000;
+        InputStream in = null;
+        try {
+            in = OpenHttpConnection(URL);
+        } catch (IOException e1) {
+            Toast.makeText(this, e1.getLocalizedMessage(),
+                    Toast.LENGTH_LONG).show();
+            e1.printStackTrace();
+            return "";
+        }
+        InputStreamReader isr = new InputStreamReader(in);
+        int charRead;
+        String str = "";
+        char[] inputBuffer = new char[BUFFER_SIZE];
+        try {
+            while ((charRead = isr.read(inputBuffer))>0)
+            {
+//---convert the chars to a String---
+                String readString = String.copyValueOf(inputBuffer, 0, charRead);
+                str += readString;
+                inputBuffer = new char[BUFFER_SIZE];
+            }
+            in.close();
+        } catch (IOException e) {
+            Toast.makeText(this, e.getLocalizedMessage(),
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+
+            return "";
+        }
+        return str;
+    }
+
+
+    public class BackgroundThings extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+            String all_products = DownloadText(url[0]);
+            return all_products;
+        }
+
+        @Override
+        protected void onPostExecute(String all_products) {
+
+
+
+            if(!all_products.equals("")){
+                String[] one_product_line = all_products.split("<br>");
+
+                int x,y;
+                String prod_names[]=new String[one_product_line.length];
+                String prod_prices[]=new String[one_product_line.length];
+                String prod_images[]=new String[one_product_line.length];
+
+                for(x=0; x<one_product_line.length; x++) {
+
+
+                    String one_products_string = one_product_line[x];
+                    String[] one_products_details = one_products_string.split("%%");
+                    prod_names[x] = one_products_details[0];
+                    prod_prices[x] = one_products_details[1];
+                    prod_images[x] = one_products_details[2];
+
+                }
+
+
+
+
+
+
+                    CustomListAdapter adapter = new CustomListAdapter(MainActivity.this, prod_names, prod_prices, prod_images);
+                    ListView list = (ListView) findViewById(android.R.id.list);
+                    list.setAdapter(adapter);
+
+
+            }
+
+
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new BackgroundThings().execute("http://192.168.1.2/troi_one/get_products2.php");
+
+        ListView list =(ListView)findViewById(android.R.id.list);
+
+    }
+}
+
